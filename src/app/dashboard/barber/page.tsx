7617fe66bb8f209d5a3177c5d3399c672bdbed93
0xroque <kodaksixx@gmail.com>
2026-06-21 15:54:15 +0100

    feat: add sign out button to barber and client dashboards

--- src/app/dashboard/barber/page.tsx
+++ src/app/dashboard/barber/page.tsx
@@ -1,6 +1,16 @@
-export default function BarberDashboard(){
-    return(<div className="div">
-        <h1>Barber Dashbord</h1>
+import { signOut } from "@/lib/auth";
+export default function BarberDashboard() {
+  return (
+    <div className="div">
+      <h1>Barber Dashbord</h1>
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <button type="submit">Sign Out</button>
+      </form>
     </div>
-)}
-    
+  );
+}
--- src/proxy.ts
+++ src/proxy.ts
@@ -1,27 +1,30 @@
-
-import { auth } from "@/lib/auth"
-import { NextResponse } from "next/server"
+import { auth } from "@/lib/auth";
+import { NextResponse } from "next/server";
 
 export default auth((req) => {
-
-if (!req.auth) {
-    return NextResponse.redirect(new URL("/api/auth/signin", req.url))
+  if (!req.auth) {
+    return NextResponse.redirect(new URL("/api/auth/signin", req.url));
   }
-  const role = req.auth?.user.role
-const isClient = role === "CLIENT"
-const isBarber = role === "BARBER"
+  const role = req.auth?.user.role;
+  const isClient = role === "CLIENT";
+  const isBarber = role === "BARBER";
 
-if(isClient && req.nextUrl.pathname.startsWith("/dashboard/barber")){
-  return NextResponse.redirect(new URL("/dashboard/client", req.url))
-}
+  if (req.nextUrl.pathname === "/") {
+    if (isBarber)
+      return NextResponse.redirect(new URL("/dashboard/barber", req.url));
+    if (isClient)
+      return NextResponse.redirect(new URL("/dashboard/client", req.url));
+  }
 
-if(isBarber && req.nextUrl.pathname.startsWith("/dashboard/client")){
-  return NextResponse.redirect(new URL("/dashboard/barber",req.url))
-}
+  if (isClient && req.nextUrl.pathname.startsWith("/dashboard/barber")) {
+    return NextResponse.redirect(new URL("/dashboard/client", req.url));
+  }
 
-})
+  if (isBarber && req.nextUrl.pathname.startsWith("/dashboard/client")) {
+    return NextResponse.redirect(new URL("/dashboard/barber", req.url));
+  }
+});
 
- 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-}
+};
