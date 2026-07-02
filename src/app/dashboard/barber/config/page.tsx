df8d1606b705c52b87793713f857894635171022
0xroque <kodaksixx@gmail.com>
2026-07-02 20:21:16 +0100

    feat: add barber config form with server action

---
+++ src/app/dashboard/barber/config/page.tsx
@@ -0,0 +1,45 @@
+import { prisma } from "@/lib/prisma";
+import { auth } from "@/lib/auth";
+
+export default function schedule() {
+  return (
+    <div>
+      <h1>Configuration</h1>
+      <form
+        action={async (formData) => {
+          "use server";
+          const session = await auth();
+          const barberId = session?.user?.id as string
+          const weekdayStart = formData.get("weekdayStart") as string;
+          const weekdayEnd = formData.get("weekdayEnd") as string;
+          const weekendStart = formData.get("weekendStart") as string;
+          const weekendEnd = formData.get("weekendEnd") as string;
+
+          await prisma.barberConfig.create({
+            data: {
+              barberId: barberId,
+              weekdayStart: weekdayStart,
+              weekdayEnd: weekdayEnd,
+              weekendStart: weekendStart,
+              weekendEnd: weekendEnd,
+            },
+          });
+        }}
+      >
+        <label htmlFor="weekdayStart">Hórario entrada dias de semana (Seg-Sex)</label>
+        <input id="weekdayStart" name="weekdayStart" type="time"></input>
+
+        <label htmlFor="weekdayEnd">Hórario saída dias de semana (Seg-Sex)</label>
+        <input id="weekdayEnd" name="weekdayEnd" type="time"></input>
+
+        <label htmlFor="weekendStart">Hórario entras Fim de semana</label>
+        <input id="weekendStart" name="weekendStart" type="time"></input>
+
+        <label htmlFor="weekendEnd">Hórario de saída Fim de semana</label>
+        <input id="weekendEnd" name="weekendEnd" type="time"></input>
+
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  );
+}
